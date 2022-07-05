f45e9cacef3b152f195fb28c0305dade713357f5
Taras Noga <taraskin2012@gmail.com>
2022-07-05 21:19:12 +0300

    add pagination to CharList

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=30907c3b6ed04578e6fe8370ae9c325f";
+  _baseOffset = 210;
 
   getResourse = async (url) => {
     let res = await fetch(url);
@@ -11,9 +12,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResourse(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
     );
     return res.data.results.map(this._transformCharacter);
   };
