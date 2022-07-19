7d284db94f8d88184d029080a45047292f564642
Taras Noga <taraskin2012@gmail.com>
2022-07-19 12:57:09 +0300

    add react router

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,22 +1,31 @@
-import './appHeader.scss';
+import { Link, NavLink } from "react-router-dom";
+import "./appHeader.scss";
 
 const AppHeader = () => {
-    return (
-        <header className="app__header">
-            <h1 className="app__title">
-                <a href="https://www.google.com.ua/">
-                    <span>Marvel</span> information portal
-                </a>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    <li><a href="https://www.google.com.ua/">Characters</a></li>
-                    /
-                    <li><a href="https://www.google.com.ua/">Comics</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="app__header">
+      <h1 className="app__title">
+        <Link to="/">
+          <span>Marvel</span> information portal
+        </Link>
+      </h1>
+      <nav className="app__menu">
+        <ul>
+          <li>
+            <NavLink exact activeStyle={{ color: "red" }} to="/">
+              Characters
+            </NavLink>
+          </li>
+          /
+          <li>
+            <NavLink exact activeStyle={{ color: "red" }} to="/comics">
+              Comics
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default AppHeader;
